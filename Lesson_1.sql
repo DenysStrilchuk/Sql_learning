00b5587c7e419530d032d69bb591e5e1721cb139
Denys Sttrilchuk <denia876@gmail.com>
2024-01-16 13:49:24 +0200

    practise sql lesson1

--- Lesson_1.sql
+++ Lesson_1.sql
@@ -14,7 +14,15 @@ select * from cars limit 5;
 select * from cars order by id desc limit 5;
 
 select avg(price) as avgPrice from  cars where model = 'KIA';
-select avg(price) as avgPrice from cars;
+select model, avg(price) as avgPrice from cars group by model;
+select model, count(*) as sumAllModels from cars group by model;
+select model, count(*) as modelCount from cars group by model order by modelCount limit 1;
+
+select * from cars where substring(model, 2, 1) = 'a' and substring(model, length(model) -  1, 1) = 'a';
+select * from cars where length(model) > 8;
+
+select * from cars where price > (select avg(price) from cars);
+
 
 
 
